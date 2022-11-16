7ddfadafe0e493399a79974c315c743c6bd318c6
natogues <gues.nato@hotmail.com>
2022-11-16 15:38:00 -0300

    foio

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>texto de abertura</Text>
+      <Text>texto de aberturasdsadaa!</Text>
       <StatusBar style="auto" />
     </View>
   );
