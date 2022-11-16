c4c8779d4ecdf0d3585e10c958c0b7f9207a51f6
danielwalterrodrigues <danielwalterrodrigues@gmail.com>
2022-11-16 15:42:06 -0300

    sdfsdf

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>texto de aberturasdsadaa!</Text>
+      <Text>texto de aberturas</Text>
       <StatusBar style="auto" />
     </View>
   );
