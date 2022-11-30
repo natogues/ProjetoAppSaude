8b1bd6ede00c51ac33bb3fd80ee965bdff57e459
danielwalterrodrigues <danielwalterrodrigues@gmail.com>
2022-11-30 16:15:32 -0300

    akjsfhdkjashd

--- componentes/Tela2.js
+++ componentes/Tela2.js
@@ -5,20 +5,24 @@ import {Text, View,StyleSheet} from 'react-native';
 const Tela2 = () => {
 
         return(           
-            <View>
-                <Text style={styles.Matheus}>{'\n'}Diga isso para o teimoso MATHEUS!!!</Text>
+            <View style={styles.bordaMatheus}>
+                <Text style={styles.Matheus}>Diga isso para o teimoso MATHEUS!!!</Text>
             </View>
               
            )
-
         
                 } 
- const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({
 Matheus: {
+    fontSize: 19,
+    color: '#ffcc00',
+},
+bordaMatheus: {
     padding:10,
-    margin:10,
-    border: 1,
+    margin: 10,
     borderColor: 'blue',
+    borderWidth: 1,
 }
 
 
