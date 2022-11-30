ebeb542c2765dbe1b427ae9b2050e971ab2be61d
natogues <gues.nato@hotmail.com>
2022-11-30 16:08:01 -0300

    xyz

--- componentes/Tela2.js
+++ componentes/Tela2.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View,StyleSheet} from 'react-native';
 
 
 const Tela2 = () => {
 
         return(           
             <View>
-                <Text>{'\n'}Diga isso para o teimoso MATHEUS!!!</Text>
+                <Text style={styles.Matheus}>{'\n'}Diga isso para o teimoso MATHEUS!!!</Text>
             </View>
               
            )
 
         
                 } 
-                
+ const styles = StyleSheet.create({
+Matheus: {
+    padding:10,
+    margin:10,
+    border: 1,
+    borderColor: 'blue',
+}
+
+
+ })               
+
                 
                 
 export default Tela2
